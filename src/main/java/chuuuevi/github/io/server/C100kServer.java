44617517acfd57faa1fdabe07829260a67bc19d4
chuuuevi <chuuuevi@gmail.com>
2024-07-13 23:59:33 +0800

    feat: fix

--- src/main/java/chuuuevi/github/io/server/C100kServer.java
+++ src/main/java/chuuuevi/github/io/server/C100kServer.java
@@ -5,12 +5,15 @@ import org.apache.commons.cli.*;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
+import java.util.Arrays;
 import java.util.concurrent.Executors;
 
 public class C100kServer {
 
 
     public static void main(String[] args) throws IOException, ParseException {
+
+        System.out.println("args="+ Arrays.toString(args));
 
         Options options = new Options();
 
