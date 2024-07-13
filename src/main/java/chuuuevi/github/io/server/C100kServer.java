28525940fb59eeb59b443e70bfb349f2a1ae5e98
chuuuevi <chuuuevi@gmail.com>
2024-07-13 23:37:20 +0800

    feat: save

--- src/main/java/chuuuevi/github/io/server/C100kServer.java
+++ src/main/java/chuuuevi/github/io/server/C100kServer.java
@@ -1,6 +1,7 @@
 package chuuuevi.github.io.server;
 
 import com.sun.net.httpserver.HttpServer;
+import org.apache.commons.cli.*;
 
 import java.io.IOException;
 import java.net.InetSocketAddress;
@@ -8,17 +9,28 @@ import java.util.concurrent.Executors;
 
 public class C100kServer {
 
-    public static void main(String[] args) throws IOException {
+
+    public static void main(String[] args) throws IOException, ParseException {
+
+        Options options = new Options();
+
+        options.addOption(null, "http-port", true, "HTTP Port");
+        CommandLineParser parser = new DefaultParser();
+        CommandLine cmd = parser.parse(options, args);
+
+        int port = cmd.hasOption("http-port") ? Integer.parseInt(cmd.getOptionValue("http-port")) : 8080;
+
         var body = "hello world".getBytes();
-        var server = HttpServer.create(new InetSocketAddress(8080), 0);
+        var server = HttpServer.create(new InetSocketAddress(port), 0);
         server.setExecutor(Executors.newVirtualThreadPerTaskExecutor());
         server.createContext("/").setHandler(exchange -> {
+//            String text = IOUtils.toString(exchange.getRequestBody(), StandardCharsets.UTF_8);
             exchange.sendResponseHeaders(200, body.length);
             try (var os = exchange.getResponseBody()) {
                 os.write(body);
             }
         });
         server.start();
-        System.out.println("ready");
+        System.out.println("port=" + port);
     }
 }
